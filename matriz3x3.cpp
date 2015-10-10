105bad293619cb92490a7360e07d670146087523
Efren Gaspar Felipe <efre.gaspar.sc@itszapopan.edu.mx>
2015-10-09 21:06:33 -0500

    Efren Gaspar

---
+++ matriz3x3.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int matrizA[3][3];
+	int matrizB[3][3];
+	int matrizR[3][3];
+	int cont1, cont2, cont3;
+
+	for (cont1=0; cont1<3; cont1++)
+		for (cont2=0; cont2<3; cont2++)
+		{
+			cout<<"Ingrese el valor de la primera matriz en la posicion: Fila "<<cont1+1<<", Columna "<<cont2+1<<" ";
+			cin>>matrizA[cont1][cont2];
+			cout<<endl;
+		}			
+	for (cont1=0; cont1<3; cont1++)
+                for (cont2=0; cont2<3; cont2++)
+                {
+			cout<<"Ingrese el valor de la segunda matriz en la posicion: Fila "<<cont1+1<<", Columna "<<cont2+1<<" ";
+                        cin>>matrizB[cont1][cont2];
+			matrizR[cont1][cont2]=0;
+                        cout<<endl;
+		}
+	for (cont1=0; cont1<3; cont1++)
+	{
+                for (cont2=0; cont2<3; cont2++)
+		{
+			for (cont3=0; cont3<3; cont3++)
+			{
+				matrizR[cont1][cont2]+=matrizA[cont1][cont3]*matrizB[cont3][cont2];
+			}
+			cout<<matrizR[cont1][cont2]<<" ";
+		}
+		cout<<endl;
+	}			
+ 	
+                	
+}
